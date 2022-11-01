2f8c5b48fbc7376e0800e34743bcd6761c109266
JoaoCardoso00 <jvcq13@gmail.com>
2022-10-31 21:06:02 -0300

    variable name refactoring

--- utils.py
+++ utils.py
@@ -2,12 +2,11 @@ from requests import get
 import os
 import shutil
 
-current_directory = os.getcwd()
-final_directory = os.path.join(current_directory, r'anexos')
+CURRENT_DIRECTORY = os.getcwd()
 
 def baixar_anexo(url: str, file_name: str, subdir: str) -> None:
     """Function to download a file from a given url and save it in a sub-directory"""
     print(f"Downloading {url}...")
-    with open(os.path.join(current_directory, subdir, file_name), 'wb') as f:
+    with open(os.path.join(CURRENT_DIRECTORY, subdir, file_name), 'wb') as f:
         f.write(get(url).content)
 
